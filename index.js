d355a09d1c5de664099685564643ffba55a97157
Abdul Hadi <hadi2408466@gmail.com>
2024-05-26 13:36:12 +0500

    update

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-
+import cors from "cors";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -31,9 +37,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
-// app.get("/api/config/paypal", (req, res) => {
-//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-// });
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
